49a2593108f0a6286fea00896b8235f4f3ff7057
Ilari Korhonen <ilarik@kth.se>
2018-03-12 11:36:24 +0100

    template main.sh

--- etc/pdcfw/main.sh
+++ etc/pdcfw/main.sh
@@ -1,11 +1,4 @@
-# pdcfw - manages PDC Linux Netfilter/IPTables firewall
-
-# /etc/pdcfw/main - pdcfw main
-# Author: Ilari Korhonen, KTH Royal Institute of Technology
-#
-# Copyright (C) 2018 KTH Royal Institute of Technology. All rights reserved.
-# See LICENSE file for more information.
-
+#!/bin/bash
 
 function main()
 {
@@ -14,32 +7,27 @@ function main()
 
     # rule chain: INPUT
     # description: incoming, packets destined to this host
-    
     # default policy: ACCEPT
     set_default_policy INPUT ACCEPT
-    
+
     # allow all from trusted interfaces, established connections, icmp w/ limits
     allow_trusted_interfaces INPUT
     allow_established INPUT
     allow_icmp_with_limits INPUT
 
     # allow SSH connections
-    allow with INPUT proto tcp from any to $(hostname) dport 22 stateful
+    allow with INPUT proto tcp from any to ${myaddr} dport 22 stateful
 
     # drop the rest
     drop_and_log_all INPUT
 
-
     # rule chain: OUTPUT
     # description: outgoing, packets generated on this host
-
     # default policy: ACCEPT
     set_default_policy OUTPUT ACCEPT
 
-
     # rule chain: FORWARD
     # description: routing, packets destined to be routed
-
     # default policy: ACCEPT
     set_default_policy FORWARD ACCEPT
 
